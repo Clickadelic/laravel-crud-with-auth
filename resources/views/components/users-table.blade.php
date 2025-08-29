ce5165cd4c0c0239c744ce4c15583e65a4dd32b3
Clickadelic <toby.hopp@gmail.com>
2025-08-29 18:42:39 +0200

    users

--- resources/views/components/users-table.blade.php
+++ resources/views/components/users-table.blade.php
@@ -4,18 +4,29 @@
     <thead>
         <tr>
             <th class="text-left">Name</th>
-            <th class="text-left">Col 2</th>
-            <th class="text-left">Col 3</th>
-            <th class="text-left">Col 4</th>
+            <th class="text-left">E-Mail</th>
+            <th class="text-left">Rolle</th>
+            <th class="text-right">Aktion</th>
         </tr>
     </thead>
     <tbody>
-        <tr>
-            <td>Max Mustermann</td>
-            <td>Col 2</td>
-            <td>Col 3</td>
-            <td>Col 4</td>
-        </tr>
+        @forelse($users as $user)
+            <tr>
+                <td>{{ $user->name }}</td>
+                <td>{{ $user->email }}</td>
+                <td><span class="rounded-sm bg-sky-600 text-white text-xs px-2 py-1">#User</span></td>
+                <td class="text-right">
+                    <x-button variant="ghost" size="sm">
+                        {{ __('Bearbeiten') }}
+                    </x-button>
+                    <x-button variant="danger" size="sm">
+                        {{ __('Löschen') }}
+                    </x-button>
+                </td>
+            </tr>
+        @empty
+            
+        @endforelse
     </tbody>
     <tfoot>
         <tr class="text-center">
