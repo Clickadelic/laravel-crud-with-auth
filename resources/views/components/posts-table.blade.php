ea019e4f5718f9ed2deb0bcb4c9ada32a522921a
Clickadelic <toby.hopp@gmail.com>
2025-08-29 21:55:53 +0200

    Responsive PostTable

--- resources/views/components/posts-table.blade.php
+++ resources/views/components/posts-table.blade.php
@@ -4,9 +4,9 @@
     <thead>
         <tr>
             <th class="py-2 text-left">Titel</th>
-            <th class="py-2 text-left">Content</th>
-            <th class="py-2 text-left">von</th>
-            <th class="py-2 text-left">Erstellt am</th>
+            <th class="py-2 text-left invisible sm:visible">Content</th>
+            <th class="py-2 text-left invisible sm:visible">von</th>
+            <th class="py-2 text-left invisible sm:visible">Erstellt am</th>
             <th class="py-2 text-right pr-4">Aktion</th>
         </tr>
     </thead>
@@ -14,10 +14,10 @@
         @forelse($posts as $post)
             <tr>
                 <td class="py-2 text-left">{{ $post->title }}</td>
-                <td class="py-2 text-left">{{ $post->content }}</td>
-                <td class="py-2 text-left">{{ $post->user_id }}</td>
-                <td class="py-2 text-left">{{ $post->created_at->format('d.m.Y H:i') }} Uhr</td>
-                <td class="py-2 text-right">
+                <td class="py-2 text-left invisible sm:visible">{{ $post->content }}</td>
+                <td class="py-2 text-left invisible sm:visible">{{ $post->user_id }}</td>
+                <td class="py-2 text-left invisible sm:visible">{{ $post->created_at->format('d.m.Y H:i') }} Uhr</td>
+                <td class="py-2 text-right flex flex-row justify-end items-end gap-2">
                     <x-link-button href="{{ route('posts.edit', $post) }}" variant="" size="sm" title="{{ __('Bearbeiten') }}">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
                             <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,7 +15,8 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('posts.index')" :active="request()->routeIs('posts.index')">
+                    {{-- Check ob wir auf "/posts" sind oder "/posts/create" oder "/posts/{id}/edit"--}}
+                    <x-nav-link :href="route('posts.index')" :active="request()->routeIs('posts.index') || request()->routeIs('posts.create') || request()->routeIs('posts.edit')">
                         {{ __('Posts') }}
                     </x-nav-link>
                     <x-nav-link :href="route('users.index')" :active="request()->routeIs('users.index')">
@@ -75,6 +76,12 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('posts.index')" :active="request()->routeIs('posts.index')">
+                {{ __('Posts') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('users.index')" :active="request()->routeIs('users.index')">
+                {{ __('Users') }}
             </x-responsive-nav-link>
         </div>
 
